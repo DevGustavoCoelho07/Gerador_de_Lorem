8f4ba7bb8f66dd1dd02b7c6004c174687a6385eb
DevGustavoCoelho07 <84469800+DevGustavoCoelho07@users.noreply.github.com>
2022-08-22 16:45:51 -0300

    mais remoção de código desnecessário e melhora no css

--- generator.js
+++ generator.js
@@ -2,27 +2,27 @@ async function generator() {
   var loremInput = document.getElementById("loremInput");
 
   paras = loremInput.value;
-  await axios
-    .get(
-      `https://baconipsum.com/api/?type=all-meat&paras=${paras}&start-with-lorem=1`
-    )
-    .then((res) => {
-      console.log(paras);
-      let dt = res.data;
-      let paragrafo = document.getElementById("pp");
+  try {
+    await axios
+      .get(
+        `https://baconipsum.com/api/?type=all-meat&paras=${paras}&start-with-lorem=1`
+      )
+      .then((res) => {
+        // let dt = res.data;
+        let paragrafo = document.getElementById("pp");
+        let item = document.createElement("div");
+        // paragrafo.classList.add("p-lorem")
+        // item.classList.add("p-lorem")
+        paragrafo.innerHTML = "";
+        // paragrafo.appendChild(item)
 
-      console.log(res.paras);
-      let item = document.createElement("div");
-      // paragrafo.classList.add("p-lorem")
-
-      // item.classList.add("p-lorem")
-      paragrafo.innerHTML = "";
-      // paragrafo.appendChild(item)
-
-      for (item of res.data) {
-        paragrafo.innerHTML = paragrafo.innerHTML + `<p>${item}</p>`;
-      }
-    });
+        for (item of res.data) {
+          paragrafo.innerHTML = paragrafo.innerHTML + `<p>${item}</p>`;
+        }
+      });
+  } catch (err) {
+    document.getElementById("pp").innerHTML = `${err}`;
+  }
 }
 
 generator();
